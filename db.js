b3d96719ff7d361e585b804fb2f9fd5ba0585b3b
kavinskarasu <kavinskarasu@gmail.com>
2023-11-21 20:27:39 +0530

    updated

--- db.js
+++ db.js
@@ -2,8 +2,6 @@ const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 dotenv.config();
 function connect() {
-  mongoose.set("useCreateIndex", true);
-
   mongoose.connect(process.env.DB, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
